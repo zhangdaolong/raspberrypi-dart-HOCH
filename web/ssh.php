a1f285c083348449ee1424ec01aad8f2fa8da888
zhangdaolong <zhangdaolong@fiberhome.com>
2018-11-14 10:13:41 +0800

    add clean data

--- web/ssh.php
+++ web/ssh.php
@@ -6,6 +6,12 @@ $pass="toor";
 $connection=ssh2_connect('127.0.0.1',22);
 ssh2_auth_password($connection,$user,$pass); 
 
+$link = mysqli_connect("127.0.0.1", "root", "toor", "sensors");
+if (!$link)
+{
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 if ($name=="system"){
      $cmd="/sbin/shutdown";
      echo "Shutdown system\n";
@@ -15,12 +21,16 @@ if ($name=="system"){
 }elseif($name=="collector"){
      $cmd="/usr/bin/killall collector;/usr/bin/collector";
      echo "Restart collector\n";
+}elseif($name=="clean"){
+     mysqli_query($link, "delete from HOCH;");
+     mysqli_close($link);
+     echo "Clean table \n";
+     exit 1;
 }else{
     $cmd=$name;
     echo "Excute ".$cmd."\n";
 }
 $ret=ssh2_exec($connection,$cmd); 
 stream_set_blocking($ret, true);
-
 echo (stream_get_contents($ret));
 ?>
